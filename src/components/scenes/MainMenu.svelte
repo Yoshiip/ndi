27b846fb9719cc2eca308b9182f8f03f4f702a6b
ThomasGysemans <gysemansthomas@gmail.com>
2024-12-05 20:58:31 +0100

    adding Zael's first svelte component

--- src/components/scenes/MainMenu.svelte
+++ src/components/scenes/MainMenu.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
-    import { game } from "$lib/game.svelte";
-
-    function startGame() {
-        game.currentScene = "game";
-    }
+    import PlayButton from "../PlayButton.svelte";
 </script>
 
 <div class="fixed top-0 left-0 w-full h-dvh flex flex-col items-center justify-center space-y-4">
     <h2 class="font-bold text-3xl">Titre du jeu banger</h2>
-    <button type="button" class="btn btn-primary" onclick={startGame}>
-        Jouer maintenant
-    </button>
+    <PlayButton />
 </div>
 
 <div class="fixed left-[50%] -translate-x-[50%] bottom-4 text-center">
