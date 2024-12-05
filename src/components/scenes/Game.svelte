80d432877ebbbf6606353cd15a8f1666bd33fec7
ThomasGysemans <gysemansthomas@gmail.com>
2024-12-05 20:31:43 +0100

    height 100vh

--- src/components/scenes/Game.svelte
+++ src/components/scenes/Game.svelte
@@ -1,11 +1,9 @@
-<script>
+<script lang="ts">
     import GameCanvas from "../GameCanvas.svelte";
     import GameSide from "../GameSide.svelte";
-
 </script>
 
-<div class="flex flex-row h-full">
+<div class="flex h-full">
     <GameCanvas />
-    
     <GameSide />
 </div>
